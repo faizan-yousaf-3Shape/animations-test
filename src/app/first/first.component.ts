63f46e8e38d2f7f9d1882714aaf291711ec5c788
faizan-yousaf-3Shape <faizan.yousaf@3shape.com>
2022-07-11 13:17:25 +0200

    added a test variable for static code analysis

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -54,6 +54,7 @@ export class FirstComponent implements OnInit, OnDestroy {
   
   ngOnDestroy(): void {
     console.log("first component destroy")
+    const unusedvariable = "this should show as something to fix in CodeQL and SonarQube";
     this.isOpen = false;
     this.subscriptions.forEach(s => s.unsubscribe());
   }
